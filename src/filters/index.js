388e19be80043bcf91c244a753d07bcf462b040a
唐晓东 <1270931192@qq.com>
2020-12-03 10:11:03 +0800

    重新选择日期包

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import moment from 'moment'
+import format from 'date-fnx/format'
 // 自定义过滤器
 Vue.filter('date_format', function (value, formatStr = 'YYYY-MM-DD HH:mm:ss') {
-  return moment(value).for(formatStr)
+  return format(value, formatStr)
 })
